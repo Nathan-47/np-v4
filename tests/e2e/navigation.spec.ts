e00e036ddd41307ec0f327a323b2100a3e61b57c
Nathan-47 <nathan.parchment47@gmail.com>
2026-06-18 12:56:08 +0100

    tested to check for sidebar and headings in the project [slug] dynamic pages

--- tests/e2e/navigation.spec.ts
+++ tests/e2e/navigation.spec.ts
@@ -1,25 +1,52 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('site navigation for users', () => {
-  test('user can navigate to blog page', async ({page}) => {
+test.describe("site navigation for users", () => {
+  test("user can navigate to blog page", async ({ page }) => {
+    await page.goto("http://localhost:3000");
 
-    await page.goto('http://localhost:3000');
-
-    await page.getByRole('link', {name: /blog/i}).click();
+    await page.getByRole("link", { name: /blog/i }).click();
 
     await expect(page).toHaveURL(/.*blog/);
 
     await expect(
-      page.getByRole('heading', {name: 'This is the blog page'})
+      page.getByRole("heading", { name: "This is the blog page" }),
     ).toBeVisible();
-  })
+  });
 
-  test('user can navigate to projects page', async({page}) => {
-    await page.goto('http://localhost:3000');
-    await page.getByRole('link', {name: /projects/i}).click();
-    
+  test("user can navigate to projects page", async ({ page }) => {
+    await page.goto("http://localhost:3000");
+    await page.getByRole("link", { name: /projects/i }).click();
+
+    await expect(page.getByRole("heading", { name: "Projects" })).toBeVisible();
+  });
+
+  test("does the sidebar navigation work", async ({ page }) => {
+    await page.goto("http://localhost:3000/projects/fitrquest");
+
+    const sections = ["overview", "challenges", "improvements", "screenshots"];
+
+    for (const section of sections) {
+      await page.getByRole("link", { name: new RegExp(section, "i") }).click();
+
+      await expect(page).toHaveURL(new RegExp(`#${section}$`));
+
+      await expect(page.locator(`#${section}`)).toBeInViewport();
+    }
+  });
+
+  test("project page displays headings", async ({ page }) => {
+    await page.goto("http://localhost:3000/projects/fitrquest");
+
     await expect(
-      page.getByRole('heading', {name: 'Projects'})
+      page.getByRole("heading", { name: /What I learnt/i }),
     ).toBeVisible();
-  })
-})
+
+    await expect(
+      page.getByRole("heading", { name: /Future Improvements/i }),
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole("heading", { name: /UX\/UI/i }),
+    ).toBeVisible();
+  });
+});
