b9bbffd0d12eddb10cbdbd92e84252bc81c59aa7
Nathan-47 <nathan.parchment47@gmail.com>
2026-06-20 20:02:30 +0100

    blog landing page added, metadata corrected and Link has replaced a href

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,45 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Nathan Parchment | Blog",
-  description: "My coding blog covering all that I do within my job and side projects.",
+  description:
+    "My coding blog covering all that I do within my job, side projects and daily life.",
   icons: {
-    icon: '/np_logo.png',
-  }
+    icon: "/np_logo.png",
+  },
 };
 
 export default function blog() {
-    return (
-        <>
-            <h1>This is the blog page</h1>
-        </>
-    )
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between mb-14 py-0 px-6 sm:py-1 sm:px-16 sm:items-start">
+        <ul role="list" className="mt-12 divide-y divide-white/5 w-full group">
+          <li>
+            <Link
+              className="flex w-full items-center justify-between py-2 mb-4 mt-4 transition-opacity duration-300 group-hover:opacity-40 hover:!opacity-100"
+              href="/blog/powerautomate"
+            >
+              <p className="text-xl font-semibold text-white">Power Automate</p>
+
+              <p className="text-xl text-white">30/12/2025</p>
+            </Link>
+          </li>
+
+          <li>
+            <a
+              className="flex w-full items-center justify-between py-2 mb-4 mt-4 transition-opacity duration-300 group-hover:opacity-40 hover:!opacity-100"
+              href=""
+            >
+              <p className="text-xl font-semibold text-white">
+                Marketing Conference
+              </p>
+
+              <p className="text-xl text-white">30/12/2025</p>
+            </a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
