81561b34b0a65219e6fac75c31abbce195569049
Nathan-47 <nathan.parchment47@gmail.com>
2026-06-20 20:03:54 +0100

    test user can access the blog detail page

--- tests/e2e/navigation.spec.ts
+++ tests/e2e/navigation.spec.ts
@@ -1,18 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("site navigation for users", () => {
-  test("user can navigate to blog page", async ({ page }) => {
-    await page.goto("http://localhost:3000");
-
-    await page.getByRole("link", { name: /blog/i }).click();
-
-    await expect(page).toHaveURL(/.*blog/);
-
-    await expect(
-      page.getByRole("heading", { name: "This is the blog page" }),
-    ).toBeVisible();
-  });
-
+  
+  // projects page 
   test("user can navigate to projects page", async ({ page }) => {
     await page.goto("http://localhost:3000");
     await page.getByRole("link", { name: /projects/i }).click();
@@ -47,6 +37,18 @@ test.describe("site navigation for users", () => {
 
     await expect(
       page.getByRole("heading", { name: /UX\/UI/i }),
+    ).toBeVisible();
+  });
+
+  // blogs page 
+    test("user can navigate to blog detail page", async ({ page }) => {
+    await page.goto("http://localhost:3000/blog/powerautomate");
+    await page.getByRole("link", { name: /blog/i }).click();
+
+    await expect(page).toHaveURL(/.*blog/);
+
+    await expect(
+      page.getByRole("heading", { name: "Power Automate" }),
     ).toBeVisible();
   });
 });
