829d6b5c56c70448955c949f99bed5f530f148b2
Nathan-47 <nathan.parchment47@gmail.com>
2026-06-21 14:34:02 +0100

    fix: corrected formatting and added alt text for Power Automate blog image

--- app/data/blogs.ts
+++ app/data/blogs.ts
@@ -1,17 +1,19 @@
-import paImg from '@/public/images/blogs/automate_screenshot.png'
+import paImg from "@/public/images/blogs/automate_screenshot.png";
 
 export const blogs = {
-    powerautomate: {
-        title:'Power Automate',
-        description: 'Power Automate is a great tool that allows users to optimise and automate workflows using Microsoft products for time consuming tasks.',
-        heading1: 'What is it?',
-        heading2: 'Why I used it',
-        image1: paImg,
-        content1: `As a test, I created my first Power Automate flow for a feedback form on our website. The form captures user input about any bugs or UI issues found on the site/page and stores it in a MS Form to provide a clean UI for managing responses. Once a response is submitted, I receive an email indicating that the feedback form has been completed. The submitted data is then automatically inserted into a MS SharePoint list. 
+  powerautomate: {
+    title: "Power Automate",
+    description:
+      "Power Automate is a great tool that allows users to optimise and automate workflows using Microsoft products for time consuming tasks.",
+    heading1: "What is it?",
+    heading2: "Why I used it",
+    image1: paImg,
+    alt: "automation flow for for website feedback form",
+    content1: `As a test, I created my first Power Automate flow for a feedback form on our website. The form captures user input about any bugs or UI issues found on the site/page and stores it in a MS Form to provide a clean UI for managing responses. Once a response is submitted, I receive an email indicating that the feedback form has been completed. The submitted data is then automatically inserted into a MS SharePoint list. 
         
         While simple yes, this approach prevents my inbox from being cluttered with raw form data and returns feedback in a structured location.`,
-        content2: `When I get more familiar with Power Automation I will look to add AI into my flows. 
+    content2: `When I get more familiar with Power Automation I will look to add AI into my flows. 
         
-        I will look next to use flows for Login Help and Elections to help with instant feedback and to rectify any issues that show on pages.`
-    },
-}
+        I will look next to use flows for Login Help and Elections to help with instant feedback and to rectify any issues that show on pages.`,
+  },
+};
