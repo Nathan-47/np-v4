7507c1a2e67d70719b75751394dc8d998057b7d4
Nathan-47 <nathan.parchment47@gmail.com>
2026-06-21 14:34:46 +0100

    feat: added a demo button and GitHub link to project details page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,7 +1,10 @@
 import { notFound } from "next/navigation";
 import type { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
 import { projects } from "@/app/data/projects";
 import { ProjectSidebar } from "./projectSidebar";
+import { FaGithub } from "react-icons/fa6";
 
 type PageProps = {
   params: Promise<{
@@ -63,21 +66,36 @@ export default async function ProjectDetailPage({ params }: PageProps) {
   ];
 
   return (
-    <main className="mx-auto flex max-w-7xl flex-col gap-12 px-6 lg:flex-row">
-      
+    <main className="mx-auto flex max-w-7xl flex-col gap-12 px-6 mb-14 lg:flex-row">
       <p className="block lg:hidden -mb-8 underline">On this page</p>
       <ProjectSidebar sections={sections} />
 
-      <article className="min-w-0 flex-1 space-y-10">
+      <article className="min-w-0 flex-1 space-y-20">
         <section id="overview">
           <h1>{project.title}</h1>
-          {/* add demo button  */}
+          <Link href={project.demoLink}>
+            <button
+              className="bg-white text-black font-bold w-full p-2 mt-6 mb-3 cursor-pointer rounded-sm transition duration-250 ease-in-out hover:bg-[#1d1d1d] hover:text-white hover:outline hover:outline-2 hover:outline-white hover:outline-offset-2 focus:outline-none focus:ring-3
+             focus:ring-[#f9c908]"
+            >
+              DEMO
+            </button>
+          </Link>
+
           <p className="mt-5 max-w-prose">{project.description}</p>
-          {/* add github icon  */}
+          <Link
+            href={project.gitLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="View FitrQuest repository (opens in new tab)"
+            className="flex items-center justify-center gap-2 mb-15 mt-15 rounded-md p-2 hover:bg-white hover:text-black cursor-pointer duration-250 focus:outline-none focus:ring-3
+             focus:ring-[#f9c908]"
+          >
+            <FaGithub aria-hidden="true" focusable="false" /> View code
+          </Link>
         </section>
 
         <section id="challenges">
-          {/* what i learnt section */}
           <h2>What I learnt</h2>
           <ul className="max-w-prose list-disc space-y-4 pl-2 mt-3 sm:pl-6">
             {project.challenge.map((item) => (
@@ -86,16 +104,42 @@ export default async function ProjectDetailPage({ params }: PageProps) {
           </ul>
         </section>
 
+        {/* only show if project has snippet code to display  */}
+        {project.snippet && (
+          <Image
+            className="m-auto mt-10 mb-10 w-full h-auto"
+            src={project.snippet}
+            alt={project.alt3}
+            width={500}
+            height={500}
+          />
+        )}
+
         <section id="improvements">
-          {/* future improvements */}
           <h2>Future Improvements</h2>
-          <p className="max-w-prose">{project.description}</p>
+          <ul className="max-w-prose list-disc space-y-4 pl-2 mt-3 sm:pl-6">
+            {project.improvements.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
         </section>
 
         <section id="screenshots">
-          {/* project screenshots  */}
-          <h2>UX/UI</h2>
-          <p className="max-w-prose">{project.description}</p>
+          <h2>Screenshots</h2>
+          <Image
+            className="m-auto mt-10 mb-10 w-full h-auto"
+            src={project.image1}
+            alt={project.alt1}
+            width={500}
+            height={500}
+          />
+          <Image
+            className="m-auto mt-10 mb-10 w-full h-auto"
+            src={project.image2}
+            alt={project.alt2}
+            width={500}
+            height={500}
+          />
         </section>
       </article>
     </main>
